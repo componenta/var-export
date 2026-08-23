3d6c6ba8606412b6802707c196a690871c74d2e0
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-23 16:15:22 +0200

    Make export policy explicit and readonly generic opt-in

--- src/Config/ExportConfig.php
+++ src/Config/ExportConfig.php
@@ -11,9 +11,7 @@ final readonly class ExportConfig
     public const int DEFAULT_MAX_DEPTH = 64;
     public const string DEFAULT_INDENT = '    ';
 
-    /**
-     * @throws ConfigurationException
-     */
+    /** @throws ConfigurationException */
     public function __construct(
         public FormatterMode $mode = FormatterMode::Standard,
         public string $indent = self::DEFAULT_INDENT,
@@ -21,6 +19,9 @@ final readonly class ExportConfig
         public bool $sortKeys = false,
         public bool $trailingComma = false,
         public ClosureUseMode $closureUseMode = ClosureUseMode::Preserve,
+        public bool $allowGenericReadonlyObjects = false,
+        public ClosureExportPolicy $closureExportPolicy = ClosureExportPolicy::SourceBound,
+        public SourcePathPolicy $sourcePathPolicy = SourcePathPolicy::AbsoluteBuildPath,
     ) {
         self::assertIndent($indent);
 
@@ -31,32 +32,47 @@ final readonly class ExportConfig
 
     public function withMode(FormatterMode $mode): self
     {
-        return new self($mode, $this->indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode);
+        return new self($mode, $this->indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode, $this->allowGenericReadonlyObjects, $this->closureExportPolicy, $this->sourcePathPolicy);
     }
 
     public function withIndent(string $indent): self
     {
-        return new self($this->mode, $indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode);
+        return new self($this->mode, $indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode, $this->allowGenericReadonlyObjects, $this->closureExportPolicy, $this->sourcePathPolicy);
     }
 
     public function withMaxDepth(int $maxDepth): self
     {
-        return new self($this->mode, $this->indent, $maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode);
+        return new self($this->mode, $this->indent, $maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode, $this->allowGenericReadonlyObjects, $this->closureExportPolicy, $this->sourcePathPolicy);
     }
 
     public function withSortKeys(bool $sortKeys = true): self
     {
-        return new self($this->mode, $this->indent, $this->maxDepth, $sortKeys, $this->trailingComma, $this->closureUseMode);
+        return new self($this->mode, $this->indent, $this->maxDepth, $sortKeys, $this->trailingComma, $this->closureUseMode, $this->allowGenericReadonlyObjects, $this->closureExportPolicy, $this->sourcePathPolicy);
     }
 
     public function withTrailingComma(bool $trailingComma = true): self
     {
-        return new self($this->mode, $this->indent, $this->maxDepth, $this->sortKeys, $trailingComma, $this->closureUseMode);
+        return new self($this->mode, $this->indent, $this->maxDepth, $this->sortKeys, $trailingComma, $this->closureUseMode, $this->allowGenericReadonlyObjects, $this->closureExportPolicy, $this->sourcePathPolicy);
     }
 
     public function withClosureUseMode(ClosureUseMode $closureUseMode): self
     {
-        return new self($this->mode, $this->indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $closureUseMode);
+        return new self($this->mode, $this->indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $closureUseMode, $this->allowGenericReadonlyObjects, $this->closureExportPolicy, $this->sourcePathPolicy);
+    }
+
+    public function withGenericReadonlyObjects(bool $allow = true): self
+    {
+        return new self($this->mode, $this->indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode, $allow, $this->closureExportPolicy, $this->sourcePathPolicy);
+    }
+
+    public function withClosureExportPolicy(ClosureExportPolicy $policy): self
+    {
+        return new self($this->mode, $this->indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode, $this->allowGenericReadonlyObjects, $policy, $this->sourcePathPolicy);
+    }
+
+    public function withSourcePathPolicy(SourcePathPolicy $policy): self
+    {
+        return new self($this->mode, $this->indent, $this->maxDepth, $this->sortKeys, $this->trailingComma, $this->closureUseMode, $this->allowGenericReadonlyObjects, $this->closureExportPolicy, $policy);
     }
 
     public static function pretty(): self
