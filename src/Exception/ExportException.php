69121b297857e610b534b7eaec9d11af9c295e90
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-23 16:15:58 +0200

    Clarify object export and cycle semantics

--- src/Exception/ExportException.php
+++ src/Exception/ExportException.php
@@ -10,9 +10,7 @@ use Throwable;
 
 class ExportException extends Exception implements ExceptionInterface
 {
-    /**
-     * @param array<string, mixed> $context
-     */
+    /** @param array<string, mixed> $context */
     public function __construct(
         string $message,
         public readonly array $context = [],
@@ -25,7 +23,6 @@ class ExportException extends Exception implements ExceptionInterface
         if ($sourceFile !== null) {
             $this->file = $sourceFile;
         }
-
         if ($sourceLine !== null) {
             $this->line = $sourceLine;
         }
@@ -34,42 +31,23 @@ class ExportException extends Exception implements ExceptionInterface
     public static function unsupportedType(mixed $value): self
     {
         $type = get_debug_type($value);
-
-        return new self(
-            sprintf('Cannot export value of type "%s".', $type),
-            ['type' => $type],
-        );
+        return new self(sprintf('Cannot export value of type "%s".', $type), ['type' => $type]);
     }
 
     public static function unexportableObject(object $object): self
     {
-        return new self(
-            sprintf(
-                'Object of type "%s" is not reconstructable as an enum or supported readonly value object.',
-                $object::class,
-            ),
-            ['class' => $object::class],
-        );
+        return new self(sprintf('Object of type "%s" is not reconstructable by the configured object exporters.', $object::class), ['class' => $object::class]);
     }
 
     public static function resourceNotExportable(mixed $resource): self
     {
-        $type = is_resource($resource)
-            ? get_resource_type($resource)
-            : (is_object($resource) ? $resource::class : 'closed resource');
-
-        return new self(
-            sprintf('Resource of type "%s" cannot be represented as executable PHP source.', $type),
-            ['resource_type' => $type],
-        );
+        $type = is_resource($resource) ? get_resource_type($resource) : (is_object($resource) ? $resource::class : 'closed resource');
+        return new self(sprintf('Resource of type "%s" cannot be represented as executable PHP source.', $type), ['resource_type' => $type]);
     }
 
     public static function objectCycle(string $class, int $depth): self
     {
-        return new self(
-            sprintf('Object graph cycle or repeated identity detected while exporting "%s" at depth %d.', $class, $depth),
-            ['class' => $class, 'depth' => $depth],
-        );
+        return new self(sprintf('Cyclic object graph detected while exporting "%s" at depth %d.', $class, $depth), ['class' => $class, 'depth' => $depth]);
     }
 
     /** @param array<int|string> $keyPath */
@@ -78,14 +56,10 @@ class ExportException extends Exception implements ExceptionInterface
         if ($keyPath === []) {
             return 'root';
         }
-
         $parts = [];
         foreach ($keyPath as $key) {
-            $parts[] = is_int($key)
-                ? sprintf('[%d]', $key)
-                : sprintf('[%s]', var_export($key, true));
+            $parts[] = is_int($key) ? sprintf('[%d]', $key) : sprintf('[%s]', var_export($key, true));
         }
-
         return '$array' . implode('', $parts);
     }
 }
