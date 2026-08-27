bb6b5513a8a0c6b1c2397d9a58241b3782fa0a40
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-27 12:56:45 +0200

    Cover split static closure declaration lines

--- tests/Regression/MultilineClosureAttributeLocationTest.php
+++ tests/Regression/MultilineClosureAttributeLocationTest.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 use Componenta\VarExport\Export;
 
-it('exports an arrow function whose attribute is on the previous line', function (): void {
+it('exports an arrow function whose attribute and static modifier precede fn', function (): void {
     $file = sys_get_temp_dir() . '/componenta_multiline_arrow_attribute_' . bin2hex(random_bytes(6)) . '.php';
     $attribute = 'ComponentaMultilineArrowAttribute' . bin2hex(random_bytes(5));
 
     try {
         file_put_contents(
             $file,
-            "<?php\n#[\\Attribute(\\Attribute::TARGET_FUNCTION)]\nclass {$attribute} {}\nreturn\n#[{$attribute}]\nstatic fn(): int => 42;\n",
+            "<?php\n#[\\Attribute(\\Attribute::TARGET_FUNCTION)]\nclass {$attribute} {}\nreturn\n#[{$attribute}]\nstatic\nfn(): int => 42;\n",
         );
         /** @var Closure $closure */
         $closure = require $file;
@@ -25,14 +25,14 @@ it('exports an arrow function whose attribute is on the previous line', function
     }
 });
 
-it('exports a closure whose multiline attribute precedes the declaration', function (): void {
+it('exports a closure whose multiline attribute and static modifier precede function', function (): void {
     $file = sys_get_temp_dir() . '/componenta_multiline_closure_attribute_' . bin2hex(random_bytes(6)) . '.php';
     $attribute = 'ComponentaMultilineClosureAttribute' . bin2hex(random_bytes(5));
 
     try {
         file_put_contents(
             $file,
-            "<?php\n#[\\Attribute(\\Attribute::TARGET_FUNCTION)]\nclass {$attribute} { public function __construct(public int \$value) {} }\nreturn\n#[{$attribute}(\n    42,\n)]\nstatic function (): int { return 42; };\n",
+            "<?php\n#[\\Attribute(\\Attribute::TARGET_FUNCTION)]\nclass {$attribute} { public function __construct(public int \$value) {} }\nreturn\n#[{$attribute}(\n    42,\n)]\nstatic\nfunction (): int { return 42; };\n",
         );
         /** @var Closure $closure */
         $closure = require $file;
@@ -53,7 +53,7 @@ it('preserves nested attributed closure magic names using the runtime declaratio
     try {
         file_put_contents(
             $file,
-            "<?php\n#[\\Attribute(\\Attribute::TARGET_FUNCTION)]\nclass {$attribute} {}\nreturn static function (): Closure {\n    return #[{$attribute}]\n    static fn(): array => [__FUNCTION__, __METHOD__];\n};\n",
+            "<?php\n#[\\Attribute(\\Attribute::TARGET_FUNCTION)]\nclass {$attribute} {}\nreturn static function (): Closure {\n    return #[{$attribute}]\n    static\n    fn(): array => [__FUNCTION__, __METHOD__];\n};\n",
         );
         /** @var Closure $outer */
         $outer = require $file;
