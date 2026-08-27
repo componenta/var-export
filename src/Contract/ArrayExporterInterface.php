b7ac57aa9df6798bd56ee1f8a76520b6c4d10b9d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-27 19:07:30 +0200

    Minimize array exporter contract

--- src/Contract/ArrayExporterInterface.php
+++ src/Contract/ArrayExporterInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Componenta\VarExport\Contract;
 
-use Componenta\VarExport\Config\ExportConfig;
-
 interface ArrayExporterInterface
 {
     /**
@@ -13,15 +11,4 @@ interface ArrayExporterInterface
      * @throws ExceptionInterface
      */
     public function export(array $array): string;
-
-    /**
-     * Advanced composition API for exporters that render an array nested in
-     * another generated expression.
-     *
-     * @param array<mixed> $array
-     * @throws ExceptionInterface
-     */
-    public function exportAtDepth(array $array, int $depth, string $baseIndent): string;
-
-    public function withConfig(ExportConfig $config): static;
 }
